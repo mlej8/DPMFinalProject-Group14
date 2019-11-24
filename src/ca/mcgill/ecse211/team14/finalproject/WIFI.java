5bb93d75b02471b55dec9170510909c48b69baeb
LORAZAROL <37741373+LORAZAROL@users.noreply.github.com>
2019-11-24 18:50:09 -0500

    launchX, launchY on intersection

--- src/ca/mcgill/ecse211/team14/finalproject/WIFI.java
+++ src/ca/mcgill/ecse211/team14/finalproject/WIFI.java
@@ -35,14 +35,14 @@ public class WIFI {
   /**
    * Variable target launch position x coordinate.
    */
-  
+
   private double binX;
-  
+
   /**
    * Variable target launch position y coordinate.
    */
   private double binY;
-  
+
   /**
    * Variable tracking if tunnel is horizontal or vertical.
    */
@@ -67,23 +67,23 @@ public class WIFI {
    * Tunnel exit Y coordinate.
    */
   private double tunnelExY;
-  
+
   /**
    * Tunnel width.
    */
-  private int tunnelWidth;
-  
+  private int tunnelWidth; // TODO: keep this?
+
   /**
    * Tunnel height.
    */
   private int tunnelHeight;
-  
+
   public WIFI() {
     setBinPosition();
     findStartPoint();
     findTunnelEnEx();
   }
-  
+
   /**
    * This methods sets the bin of current run.
    */
@@ -91,17 +91,17 @@ public class WIFI {
     if (redTeam == TEAM_NUMBER) {
       binX = redBin.x;
       binY = redBin.y;
-    } else if(greenTeam == TEAM_NUMBER) {
+    } else if (greenTeam == TEAM_NUMBER) {
       binX = greenBin.x;
       binY = greenBin.y;
-    } 
+    }
   }
 
   /**
    * This method uses the given target position (binX,binY) to find the ideal launching position.
    */
   public void findLaunchPosition() {
-    
+
     double currentX = odometer.getXYT()[0];
     double currentY = odometer.getXYT()[1];
 
@@ -109,34 +109,18 @@ public class WIFI {
 
     double dx, dy;
     // calculate the intersection of the circle and the line
-        dy = LAUNCH_RANGE * Math.cos(-theta);
-        dx = LAUNCH_RANGE * Math.sin(theta);
-        this.launchY = binY + dy;
-        this.launchX = binX + dx;
-    
+    dy = LAUNCH_RANGE * Math.cos(-theta);
+    dx = LAUNCH_RANGE * Math.sin(theta);
+    this.launchY = binY + dy;
+    this.launchX = binX + dx;
+
+    int launchx = (int) this.launchX;
+    int launchy = (int) this.launchY;
+
+    this.launchX = launchx * TILE_SIZE;
+    this.launchY = launchy * TILE_SIZE;
   }
-  
-  
-  /**
-   * 
-   * @param curPos
-   * @param center
-   * @return a point on the circle centered at center.
-   */
-  private static double[] findCircle(double[] curPos, double[] center) {
-      double[] target = new double[2];
-      if (center[0] > center[1]) { // upper half
-          double tX = curPos[0];
-          double tY = Math.sqrt(Math.pow(LAUNCH_RANGE, 2) - Math.pow((curPos[0] - center[0]), 2)) + center[1];
-          target = new double[] { tX, tY };
-      } else { // lower half
-          double tY = curPos[1];
-          double tX = Math.sqrt(Math.pow(LAUNCH_RANGE, 2) - Math.pow((curPos[1] - center[1]), 2)) + center[0];
-          target = new double[] { tX, tY };
-      }
-      return target;
-  }
-  
+
 
   /**
    * Changes the starting position (x,y).
@@ -152,26 +136,26 @@ public class WIFI {
     switch (startCorner) {
       case 0:
         startX = 1;
-        startY = 1;                         // (1,1)
+        startY = 1; // (1,1)
         startT = 90;
         break;
       case 1:
         startX = (mapWidth - 1);
-        startY = 1;                         // (14, 1)
+        startY = 1; // (14, 1)
         startT = 0;
         break;
       case 2:
-//        startX = (mapWidth - 0.5) * TILE_SIZE;
-//        startY = (mapHeight - 0.5) * TILE_SIZE;
+        // startX = (mapWidth - 0.5) * TILE_SIZE;
+        // startY = (mapHeight - 0.5) * TILE_SIZE;
         startX = (mapWidth - 1);
-        startY = (mapHeight - 1);       // (14, 8)
+        startY = (mapHeight - 1); // (14, 8)
         startT = 180;
         break;
       case 3:
-//        startX = 0.5 * TILE_SIZE;
-//        startY = (mapHeight - 0.5) * TILE_SIZE;
+        // startX = 0.5 * TILE_SIZE;
+        // startY = (mapHeight - 0.5) * TILE_SIZE;
         startX = 1;
-        startY = (mapHeight - 1);       // (1, 8)
+        startY = (mapHeight - 1); // (1, 8)
         startT = 270;
         break;
     }
@@ -188,148 +172,149 @@ public class WIFI {
     if (redTeam == TEAM_NUMBER) {
       tunnelArea = tnr;
       startArea = red;
-    } else if (greenTeam == TEAM_NUMBER){
+    } else if (greenTeam == TEAM_NUMBER) {
       tunnelArea = tng;
       startArea = green;
     }
-    
-    // TODO: Considering current start area corner's coordinate and tunnel's coordinates, compute whether tunnel is horizontal or vertical
+
+    // TODO: Considering current start area corner's coordinate and tunnel's coordinates, compute whether tunnel is
+    // horizontal or vertical
     // TODO: Compute tunnel's width and tunnel's height: width = 1, height = 2?
 
 
     double w = tunnelArea.ur.x - tunnelArea.ll.x;
     double h = tunnelArea.ur.y - tunnelArea.ll.y;
-    if(w != h) {
+    if (w != h) {
       // 2*1 tunnel
       switch (startCorner) {
         case 0:
-          if(w > h) {
+          if (w > h) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ll.x - 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ur.x + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x - 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ur.x + 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else {
+          } else {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y - 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ur.y + 1) * TILE_SIZE;
           }
           break;
         case 1:
-          if(w > h) {
+          if (w > h) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ur.x + 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ll.x - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ur.x + 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ll.x - 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else {
+          } else {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y - 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ur.y + 1) * TILE_SIZE;
           }
           break;
         case 2:
-          if(w > h) {
+          if (w > h) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ur.x + 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ll.x - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ur.x + 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ll.x - 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else {
+          } else {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ur.y + 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ll.y - 1) * TILE_SIZE;
           }
           break;
         case 3:
-          if(w > h) {
+          if (w > h) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ll.x - 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ur.x + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x - 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ur.x + 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else {
+          } else {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ur.y + 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ll.y - 1) * TILE_SIZE;
           }
           break;
       }
 
-    }else {
+    } else {
       // 1*1 tunnel
       switch (startCorner) {
         case 0:
-          if(tunnelArea.ll.x == startArea.ur.x) {
+          if (tunnelArea.ll.x == startArea.ur.x) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ll.x - 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ur.x + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x - 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ur.x + 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else if(tunnelArea.ll.y == startArea.ur.y) {
+          } else if (tunnelArea.ll.y == startArea.ur.y) {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y - 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ur.y + 1) * TILE_SIZE;
           }
           break;
         case 1:
-          if(tunnelArea.ur.x == startArea.ll.x) {
+          if (tunnelArea.ur.x == startArea.ll.x) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ur.x + 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ll.x - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ur.x + 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ll.x - 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else if(tunnelArea.ll.y == startArea.ur.y) {
+          } else if (tunnelArea.ll.y == startArea.ur.y) {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y - 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ur.y + 1) * TILE_SIZE;
           }
           break;
         case 2:
-          if(tunnelArea.ur.x == startArea.ll.x) {
+          if (tunnelArea.ur.x == startArea.ll.x) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ur.x + 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ll.x - 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ur.x + 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ll.x - 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else if(tunnelArea.ur.y == startArea.ll.y) {
+          } else if (tunnelArea.ur.y == startArea.ll.y) {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ur.y + 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ll.y - 1) * TILE_SIZE;
           }
           break;
         case 3:
-          if(tunnelArea.ll.x == startArea.ur.x) {
+          if (tunnelArea.ll.x == startArea.ur.x) {
             isTunnelHorizontal = true;
-            tunnelEnX = (tunnelArea.ll.x - 1)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ll.y + 0.5)*TILE_SIZE;
-            tunnelExX = (tunnelArea.ur.x + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x - 1) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ll.y + 0.5) * TILE_SIZE;
+            tunnelExX = (tunnelArea.ur.x + 1) * TILE_SIZE;
             tunnelExY = tunnelEnY;
-          }else if(tunnelArea.ur.y == startArea.ll.x) {
+          } else if (tunnelArea.ur.y == startArea.ll.x) {
             isTunnelHorizontal = false;
-            tunnelEnX = (tunnelArea.ll.x + 0.5)*TILE_SIZE;
-            tunnelEnY = (tunnelArea.ur.y + 1)*TILE_SIZE;
+            tunnelEnX = (tunnelArea.ll.x + 0.5) * TILE_SIZE;
+            tunnelEnY = (tunnelArea.ur.y + 1) * TILE_SIZE;
             tunnelExX = tunnelEnX;
-            tunnelExY = (tunnelArea.ll.y - 1)*TILE_SIZE;
+            tunnelExY = (tunnelArea.ll.y - 1) * TILE_SIZE;
           }
           break;
       }
     }
-   
+
   }
 
   /**
@@ -401,7 +386,7 @@ public class WIFI {
   public int getTunnelHeight() {
     return tunnelHeight;
   }
-  
+
   public int getTunnelWidth() {
     return tunnelWidth;
   }
